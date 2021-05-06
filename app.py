b837c117c5a0818caeac04fb21c76041611f1ebf
rithesh rajasekar <rithesh.rajasekar@gmail.com>
2021-05-05 22:08:03 -0400

    test123

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from os import path
-import boto3
+#import boto3
 from botocore.client import Config
 
 
