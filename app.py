faa89b376b4289ffd62f5babbe97a90afcafaed7
rithesh rajasekar <rithesh.rajasekar@gmail.com>
2021-05-05 22:37:33 -0400

    1

--- app.py
+++ app.py
@@ -91,8 +91,7 @@ def add_info():
   else:
     college_name_input = 'none'
   
-  #new_entry = Entry(first_name=first_name_input, last_name=last_name_input, email=email_input, college_name=college_name_input, job_sector=job_sector_input, blurb=blurb_input, profile_pic=profile_pic_path, approval_status=approval_status_input)
-  current_id += 1
+  new_entry = Entry(first_name=first_name_input, last_name=last_name_input, email=email_input, college_name=college_name_input, job_sector=job_sector_input, blurb=blurb_input, approval_status=approval_status_input)
   #print("currentId: " + str(current_id))
   
  return render_template('addinfo.html')
