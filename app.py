1839b1cfd51845621dee039166df31121c6a9da0
rithesh rajasekar <rithesh.rajasekar@gmail.com>
2021-05-28 16:48:59 -0400

    r

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from settings import *
 app = Flask(__name__)
 DB_NAME = "database_2.db"
 UPLOAD_FOLDER = './static/images/profile_pics'
+print(database_url)
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SECRET_KEY'] = secret_key
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
